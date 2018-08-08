455640619142ffe7a55a004f86f920dea430576a
claramarie14 <shs.claracabrera@gmail.com>
2018-08-08 16:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ const currentUser= 'Clara Cabrera';
 const welcomeMessage= `Welcome to Klossybook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
-console.log (currentUser)
